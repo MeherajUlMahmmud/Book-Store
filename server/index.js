9741b905bf607a1e4a36e88075d0d33276a7e0c5
Meheraj <57069206+MeherajUlMahmmud@users.noreply.github.com>
2023-10-28 21:04:35 +0600

    Update index.js

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const bodyParser = require("body-parser");
 const database = require("./database");
 const cors = require('cors');
+const { graphqlHTTP } = require('express-graphql');
+const { buildSchema } = require('graphql');
 
 // Routes
 const authRoutes = require("./routes/auth.route");
@@ -21,12 +23,36 @@ app.use(cors());
 const PORT = process.env.PORT || 8000;
 
 app.get('/', (req, res) => res.send('Hello world'));
-app.use("/api/auth", authRoutes);
-app.use("/api", userRoutes);
-app.use("/api", bookRoutes);
-app.use("/api", authorRoutes);
-app.use("/api", reviewRoutes);
-app.use("/api", bookBorrowRoutes);
-app.use("/api", bookReserveRoutes);
+app.use('/graphql', graphqlHTTP({
+	schema: buildSchema(`
+		type RootQuery {
+			events: [String!]!
+		}
+		type RootMutation {
+			createEvent(name: String): String
+		}
+		schema {
+			query: RootQuery,
+			mutation: RootMutation
+		}
+	`),
+	rootValue: {
+		events: () => {
+			return ['Romantic Cooking', 'Sailing', 'All-Night Coding'];
+		},
+		createEvent: (args) => {
+			const eventName = args.name;
+			return eventName;
+		}
+	},
+	graphiql: true,
+}));
+// app.use("/api/auth", authRoutes);
+// app.use("/api", userRoutes);
+// app.use("/api", bookRoutes);
+// app.use("/api", authorRoutes);
+// app.use("/api", reviewRoutes);
+// app.use("/api", bookBorrowRoutes);
+// app.use("/api", bookReserveRoutes);
 
 app.listen(PORT, () => `Server is running on port ${PORT}`);
